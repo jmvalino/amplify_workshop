893bafc3521f5353347a68060af8a2d7545ab700
jmvalino <>
2021-07-19 13:40:49 +0800

    add cypress settings

---
+++ cypress/integration/authenticator_spec.js
@@ -0,0 +1,22 @@
+describe("Authenticator:", function () {
+  beforeEach(function () {
+    cy.visit("/");
+  });
+  describe("Sign In:", () => {
+    it("allows a user to signin", () => {
+      cy.get(selectors.usernameInput).type("<Your Username in stating env>");
+      cy.get(selectors.signInPasswordInput).type(
+        "<Your Password in staging env>"
+      );
+      cy.get(selectors.signInSignInButton).contains("Sign In").click();
+      cy.wait(3000);
+      cy.get(selectors.root).contains("Global");
+    });
+  });
+});
+export const selectors = {
+  usernameInput: '[data-test="sign-in-username-input"]',
+  signInPasswordInput: '[data-test="sign-in-password-input"]',
+  signInSignInButton: '[data-test="sign-in-sign-in-button"]',
+  root: "#root",
+};
