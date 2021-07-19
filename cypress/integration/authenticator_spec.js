8348fe46c23c89da377d18850eca4853b98d78d4
jmvalino <>
2021-07-19 14:39:25 +0800

    fix: iclude test data

--- cypress/integration/authenticator_spec.js
+++ cypress/integration/authenticator_spec.js
@@ -4,10 +4,8 @@ describe("Authenticator:", function () {
   });
   describe("Sign In:", () => {
     it("allows a user to signin", () => {
-      cy.get(selectors.usernameInput).type("<Your Username in stating env>");
-      cy.get(selectors.signInPasswordInput).type(
-        "<Your Password in staging env>"
-      );
+      cy.get(selectors.usernameInput).type("jmvalino");
+      cy.get(selectors.signInPasswordInput).type("1234abcD!!");
       cy.get(selectors.signInSignInButton).contains("Sign In").click();
       cy.wait(3000);
       cy.get(selectors.root).contains("Global");
